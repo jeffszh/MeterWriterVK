14764ce58c17f50d2f7f853fca61d03239477209
Jeff <zhsu@amwares.com>
2019-12-27 16:55:14 +0800

    更改配置，回退到Vaadin13，因为Vaadin14很古怪。

---
+++ src/main/kotlin/com/amware/meterwritervk/MainView.kt
@@ -0,0 +1,34 @@
+package com.amware.meterwritervk
+
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.html.H1
+import com.vaadin.flow.component.html.Label
+import com.vaadin.flow.component.orderedlayout.FlexComponent
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.router.Route
+
+@Route("")
+class MainView : VerticalLayout() {
+
+	init {
+		defaultHorizontalComponentAlignment = FlexComponent.Alignment.CENTER
+		val versionText = Label("V0.0.1")
+		add(H1("打字練習"), versionText, NavPane())
+		setHorizontalComponentAlignment(FlexComponent.Alignment.END, versionText)
+	}
+
+	class NavPane : HorizontalLayout() {
+		init {
+			add(
+					Button("练习输入密码") {
+						ui.get().navigate("pwExercise")
+					},
+					Button("键位练习") {
+//						UI.getCurrent().navigate(KeyPosExView::class.java)
+					}
+			)
+		}
+	}
+
+}
